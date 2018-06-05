f899a329f6bb259b12d14eddcb75cc70d91f1882
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-06-05 11:27:19 -0300

    add marker with animation in the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Map extends Component {
-
   state = {
     map: null
   }
 
   componentDidMount() {
-    const { lat, lng } = this.props;
+    const { location } = this.props.place.geometry;
 
     const map = new google.maps.Map(this.refs.map, {
       zoom: 15,
-      center: {
-        lat,
-        lng
-      }
+      center: location
     });
 
-    this.setState({
-      map
+    this._createMarker(map);
+    this.setState({ map });
+  }
+
+  _setInfoWindow = (map, marker) => {
+    const infoWindow = new google.maps.InfoWindow();
+
+    infoWindow.setContent(this.props.place.name);
+    infoWindow.open(map, marker);
+  }
+
+  _createMarker(map) {
+    const marker = new google.maps.Marker({
+      map,
+      position: this.props.place.geometry.location,
+      animation: google.maps.Animation.DROP,
     });
+
+    this._setInfoWindow(map, marker);
   }
 
   update() {
